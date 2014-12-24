e7d5aa08b7de0abb3d6ad0a40548f334359d4e52
cheddar <jmg2967@rit.edu>
2014-12-24 01:51:10 -0500

    tabs -> spaces

--- app/__init__.py
+++ app/__init__.py
@@ -15,16 +15,16 @@ db = SQLAlchemy(app)
 oauth = OAuth(app)
 
 google = oauth.remote_app(
-	'google',
-	consumer_key= GOOGLE_CONSUMER_KEY,
-	consumer_secret= GOOGLE_CONSUMER_SECRET,
-	request_token_params={
-		'scope' : 'https://www.googleapis.com/auth/userinfo.email'},
-	base_url='https://www.googleapis.com/oauth2/v1/',
-	request_token_url = None,
-	access_token_method = 'POST',
-	access_token_url='https://accounts.google.com/o/oauth2/token',
-	authorize_url = 'https://accounts.google.com/o/oauth2/auth'
+    'google',
+    consumer_key= GOOGLE_CONSUMER_KEY,
+    consumer_secret= GOOGLE_CONSUMER_SECRET,
+    request_token_params={
+        'scope' : 'https://www.googleapis.com/auth/userinfo.email'},
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url = None,
+    access_token_method = 'POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url = 'https://accounts.google.com/o/oauth2/auth'
 )
 
 
@@ -36,28 +36,29 @@ lm.login_view = 'login'
 mail = Mail(app)
 
 from app import views, models
+
 """
 if not app.debug:
-	import logging
+    import logging
 
-	## Email Logging
+    ## Email Logging
 
-	from logging.handlers import SMTPHandler
-	credentials = None
-	if MAIL_USERNAME or MAIL_PASSWORD:
-		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'RC3 Site failure', credentials)
-	mail_handler.setLevel(logging.ERROR)
-	app.logger.addHandler(mail_handler)
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'RC3 Site failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
-	## File Logging
+    ## File Logging
 
-	from logging.handlers import RotatingFileHandler
-	file_handler = RotatingFileHandler('tmp/rc3.log', 'a', 1 * 1024 * 1024, 10)
-	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d]'))
-	app.logger.setLevel(logging.INFO)
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
-	app.logger.info('RC3 startup')
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/rc3.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('RC3 startup')
 
-	"""
+"""
