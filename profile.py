6c5d6e9febe73125eb8aa24a301bd4e8e71b8a76
Jaime Geiger <jmg2967@rit.edu>
2015-04-02 11:36:16 -0400

    updated profiler script with default number of functions to show

--- profile.py
+++ profile.py
@@ -3,7 +3,12 @@ from werkzeug.contrib.profiler import ProfilerMiddleware
 from app import app
 import sys
 
+if len(sys.argv) == 1:
+    num=10
+else:
+    num=sys.argv[1]
+
 app.config['PROFILE'] = True
-app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[int(sys.argv[1])])
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[int(num)])
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug = True)
