ebac588dce88f1a4d29d1d39cbc9b85c813b6291
Jaime Geiger <jmg2967@rit.edu>
2015-04-06 21:22:04 -0400

    modules set up

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 import facebook
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
@@ -35,7 +35,20 @@ lm.login_view = 'login'
 
 mail = Mail(app)
 
-from app import views, models
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', title='404'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+from app import models
+from app.views import irsec, main
+app.register_blueprint(irsec.irsec)
+app.register_blueprint(main.main)
+
 
 """
 if not app.debug:
---
+++ app/views/irsec.py
@@ -0,0 +1,8 @@
+from flask import render_template, flash, redirect, session, url_for, request, g, abort, Blueprint
+from app import app, db
+
+irsec = Blueprint('irsec', __name__, subdomain='irsec')
+
+@irsec.route('/', subdomain='irsec')
+def irsec_index():
+    return render_template('irsec/index.html')
