9a9a6d656fc8e9be718a44e6620b76cd9cbef66b
jgeigerm <jmg2967@rit.edu>
2015-06-24 23:09:59 -0400

    real life docstrings

--- app/decorators.py
+++ app/decorators.py
@@ -4,7 +4,10 @@ decorators.py - function wrappers, basically
 from threading import Thread
 
 def async(f):
-    '''spawns a new thread when the decorated function is launched'''
+    '''spawns a new thread when the decorated function is called
+    arg:
+        f - the function to thread
+    '''
     def wrapper(*args, **kwargs):
         thr = Thread(target = f, args = args, kwargs = kwargs)
         thr.start()
