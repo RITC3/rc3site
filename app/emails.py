12f6de085b90ee80a7c74e2f16d5806b28f16757
jgeigerm <jmg2967@rit.edu>
2015-06-24 23:06:42 -0400

    real life docstrings

--- app/emails.py
+++ app/emails.py
@@ -1,6 +1,8 @@
+'''
+emails.py - setup for email sending
+'''
 from flask import render_template
 from flask.ext.mail import Message
-from flask import current_app
 from config import BASE_ADMINS
 from decorators import async
 from models import User
@@ -9,11 +11,22 @@ from time import sleep
 
 @async
 def send_async_email(msg):
+    '''send an email in another thread, see decorators.py
+    arg:
+        msg - the Message object to send
+    '''
     with app.app_context():
         mail.connect()
         mail.send(msg)
 
 def send_email(subject, recipients, text_body, html_body):
+    '''setup the email to be sent
+    args:
+        subject - the subject of the email to be sent
+        recipients - the recipients of the email to be sent
+        text_body - the body of the email to be sent, in plain text
+        html_body - the body of the email to be sent, in html
+    '''
     sender = ('RC3 E-Board', 'rc3club@gmail.com')
     msg = Message(subject, sender = sender, bcc = recipients)
     msg.body = text_body
@@ -21,6 +34,10 @@ def send_email(subject, recipients, text_body, html_body):
     send_async_email(msg)
 
 def send_welcome(user):
+    '''send email upon registration
+    arg:
+        user - the user to send the welcome email to
+    '''
     send_email("Welcome to RC3 {}!".format(user.nickname),
         [user.email],
         render_template("welcome_email.txt",
@@ -28,12 +45,22 @@ def send_welcome(user):
         render_template("welcome_email.html",
             user = user))
 
-def send_newsletter(subject,body):
+def send_newsletter(subject, body):
+    '''send out the newsletter
+    args:
+        subject - the subject of the newsletter to send
+        body - the name of the newsletter to send
+    '''
     users = [ x.email for x in User.query.filter_by(newsletter = 1).all() ]
-    for chunk in [users[x:x+20] for x in range(0, len(users), 20)]:
+    #rit doesn't just let you bcc everyone, chunks of 20 work
+    chunksize = 20
+    for chunk in [users[x:x+chunksize] for x in range(0, len(users), chunksize)]:
         send_email(subject, chunk, body, body)
         sleep(0.05)
 
-
 def contact_us(msg):
+    '''send an email to the site admins
+    arg:
+        msg - the message to send
+    '''
     send_email(msg[0], BASE_ADMINS, msg[1], msg[1])
