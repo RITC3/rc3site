9bf90568d09aa6823643ce852dd12231a9e9c9fa
jgeigerm <jmg2967@rit.edu>
2016-04-12 18:33:35 -0400

    new run script

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 #!flask/bin/python
 from app import app
+from argparse import ArgumentParser
+
+def parse_and_run(args=None):
+    p = ArgumentParser()
+    p.add_argument('--bind', '-b', action='store', help='the address to bind to', default='127.0.0.1')
+    p.add_argument('--port', '-p', action='store', type=int, help='the port to listen on', default=8080)
+    p.add_argument('--debug', '-d', action='store_true', help='enable debugging (use with caution)', default=False)
+    args = p.parse_args(args)
+    app.run(host=args.bind, port=args.port, debug=args.debug)
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80, debug = True)
+    parse_and_run()
